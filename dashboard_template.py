155bb75bf0a99106a56ddd19d2cd086acd95924b
Jfrin <115793015+AyTuhEh4624@users.noreply.github.com>
2024-11-13 07:40:06 +0800

    Update dashboard_template.py
    added numpy lib import

--- dashboard_template.py
+++ dashboard_template.py
@@ -5,6 +5,7 @@ import pandas as pd
 import altair as alt
 import plotly.express as px
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
