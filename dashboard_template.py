9536544dadfbca7413c4fa210a4d7cf776fdee1b
ArjunTags <158262238+ArjunTags@users.noreply.github.com>
2024-11-13 14:06:52 +0800

    Update dashboard_template.py

--- dashboard_template.py
+++ dashboard_template.py
@@ -374,15 +374,7 @@ elif st.session_state.page_selection == "prediction":
     df.head(), df.columns
     data_filtered = df[['salary_in_usd', 'company_location']].copy()
 
-    label_encoder = LabelEncoder()
-    data_filtered.loc[:, 'company_location_encoded'] = label_encoder.fit_transform(data_filtered['company_location'])
-    
-    
-    data_filtered = data_filtered.drop(columns=['company_location'])
-    
-    
-    data_filtered.head()
-    
+    from sklearn.cluster import KMeans
     inertia = []
     k_range = range(1, 11)
     for k in k_range:
